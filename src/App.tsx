3413a0ff903a37883cb2188a564c78c8100a9b3d
John Micko <jmicko@protonmail.com>
2024-05-01 12:42:53 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import clock from '/clock-rounded.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={clock} class="logo" alt="quick timer logo" />
+        <h1>Quick Timer</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
